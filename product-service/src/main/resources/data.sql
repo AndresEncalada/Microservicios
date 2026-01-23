61efbbdfd8a31434caff7b3e9e26feef789295d8
karenop <karenorpe2004@gmail.com>
2026-01-23 14:37:38 -0500

    Implementar configuración de Kubernetes para los servicios de API Gateway, ms-ordenes, frontend, MySQL y PostgreSQL, incluyendo despliegues y servicios.

--- product-service/src/main/resources/data.sql
+++ product-service/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- data.sql
-INSERT INTO products (nombre, precio, stock) VALUES ('Laptop Gamer HP', 1200.50, 10) ON CONFLICT (id) DO NOTHING;
-INSERT INTO products (nombre, precio, stock) VALUES ('Mouse Inalámbrico', 25.00, 50) ON CONFLICT (id) DO NOTHING;
-INSERT INTO products (nombre, precio, stock) VALUES ('Teclado Mecánico', 85.99, 20) ON CONFLICT (id) DO NOTHING;
-INSERT INTO products (nombre, precio, stock) VALUES ('Monitor 24 pulgadas', 180.00, 15) ON CONFLICT (id) DO NOTHING;
-INSERT INTO products (nombre, precio, stock) VALUES ('Silla Ergonómica', 250.00, 5) ON CONFLICT (id) DO NOTHING;
+INSERT INTO products VALUES (1, 'Laptop Gamer HP', 1200.50, 10) ON CONFLICT (id) DO NOTHING;
+INSERT INTO products VALUES (2, 'Mouse Inalámbrico', 25.00, 50) ON CONFLICT (id) DO NOTHING;
+INSERT INTO products VALUES (3, 'Teclado Mecánico', 85.99, 20) ON CONFLICT (id) DO NOTHING;
+INSERT INTO products VALUES (4, 'Monitor 24 pulgadas', 180.00, 15) ON CONFLICT (id) DO NOTHING;
+INSERT INTO products VALUES (5, 'Silla Ergonómica', 250.00, 5) ON CONFLICT (id) DO NOTHING;
